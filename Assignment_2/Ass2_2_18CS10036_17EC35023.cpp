8c90bad35ab58172a99aebce541ecd9f8a56a43f
shubham-sahoo <shubhamsomnath@gmail.com>
2021-01-28 12:54:05 +0530

    Final code

---
+++ Assignment_2/Ass2_2_18CS10036_17EC35023.cpp
@@ -0,0 +1,513 @@
+#include<stdio.h> 
+#include<string.h> 
+#include<stdlib.h> 
+#include<unistd.h> 
+#include<sys/types.h> 
+#include<sys/wait.h> 
+#include<readline/readline.h> 
+#include<readline/history.h> 
+#include<iostream>
+#include<vector>
+#include <cassert> 
+#include<fcntl.h> 
+
+#define MAX_LENGTH 1024
+
+
+using namespace std;
+
+void print_direc()
+{
+	char *usr = new char[MAX_LENGTH];
+	char *hst = new char[MAX_LENGTH];
+	char *dir = new char[MAX_LENGTH];
+ 	
+
+	getlogin_r(usr, MAX_LENGTH);
+	gethostname(hst, MAX_LENGTH);
+	getcwd(dir, MAX_LENGTH);    
+
+    cout<<""<<usr<<"@"<<hst<<":"<<string(dir)<<"$ ";
+    delete(usr);
+    delete(hst);
+    delete(dir);
+}
+
+int check_pipe(string input, string* split_pipe)
+{
+	int i; 
+	char *in = &input[0];
+	char *out;
+
+	for (i = 0; i < MAX_LENGTH; i++) 
+	{ 
+		out = strsep(&in, "|");
+		if(out!=NULL)
+			split_pipe[i] = out;
+		if (out==NULL) 
+		{	
+			break; 
+		}
+	}
+	
+	if (split_pipe[1].size()==0) 
+		return 0;
+	else { 
+		return i; 
+	} 
+
+}
+
+void parse(string input, string *parsed_com)
+{
+	int i; 
+	char *in = &input[0];
+	char *out;
+	for (i = 0; i < MAX_LENGTH; i++) 
+	{ 
+		out = strsep(&in, " ");
+		if(out!=NULL)
+			parsed_com[i] = out;
+
+		if (out == NULL) 
+		{
+			break;
+		}
+		if (parsed_com[i].size() == 0)
+		{
+			i--; 
+		}
+
+	}
+
+}
+
+void parse_piped(string *split_pipe, string **parsed_pipe, int n)
+{
+	int i;
+	string *pr_val;
+	for(i=0;i<n;i++)
+	{	
+		pr_val = new string[MAX_LENGTH];
+		parse(split_pipe[i],pr_val);
+		parsed_pipe[i] = pr_val;
+	}
+
+}
+
+int split_fn(string input, string *parsed_com, string **parsed_pipe)
+{
+	string *split_pipe = new string[MAX_LENGTH];
+	
+	int pipe = 0;
+
+	pipe = check_pipe(input,split_pipe);
+
+	if(pipe>0)
+	{
+		parse_piped(split_pipe, parsed_pipe, pipe);
+	}
+	else
+	{
+		parse(split_pipe[0],parsed_com);
+	}
+	return pipe;
+}
+
+void launch_proc(string *parsed_com)
+{	
+	int st = 1;
+	for(int i=0;i<1024;i++)
+	{	
+		if(parsed_com[i].size()>0)
+		{
+			if(strcmp((char*)&parsed_com[i][0],"&")==0)
+			{
+				st = 0;
+			}
+		}
+	}
+
+	pid_t pid = fork(); 
+
+	if (pid == -1) { 
+		printf("\nFailed forking child.."); 
+		return; 
+	} 
+	else if (pid == 0) 
+	{ 
+
+		const char *input;
+		input = &parsed_com[0][0];
+
+		int j=0;
+		for(int i=0;i<1024;i++)
+		{	
+			if(parsed_com[i].size()>0)
+			{
+				j++;
+			}
+		}
+		char *full[1024];
+
+		for(int i=0;i<j;i++)
+		{		
+			full[i] = new char[parsed_com[i].size()];
+			full[i] = &parsed_com[i][0];
+		}
+
+		for (int i=j;i<1024;i++)
+		{	
+			full[i] = new char[sizeof(NULL)];
+			full[i] = NULL;
+		}
+		
+		if (execvp(input, full)< 0)  //   
+		{ 
+			printf("\nCould not execute command.."); 
+		} 
+		exit(0); 
+	} 
+
+	else if(st==0)
+	{ 
+		return; 
+	}
+	
+	else 
+	{
+		wait(NULL);
+		return;
+
+	}
+}
+
+int launch_io_redirect(string *parsed_com)
+{
+	if (parsed_com[0] == ""){	// Empty command
+		return 1;
+	}
+
+	// Copy the current Standard Input and Output file descriptors
+	// so they can be restored after executing the current command
+	int std_in, std_out, std_err;
+	std_in = dup(0);
+	std_out = dup(1);
+	std_err = dup(2);
+
+	// Check if redirection operators are present
+	int i = 1;
+
+	
+	
+	while ( parsed_com[i].size()!=0 )
+	{
+		if ( strcmp( (char*)&parsed_com[i][0], "<" ) == 0 )
+		{	// Input redirection
+			char *input = &parsed_com[i+1][0];
+			int inp = open( input, O_RDONLY );
+			
+			
+
+			if ( inp < 0 )
+			{
+				perror("minsh");
+				return 1;
+			}
+
+			if ( dup2(inp, 0) < 0 )
+			{
+				perror("minsh");
+				return 1;
+			}
+			close(inp);
+
+			parsed_com[i] = "";
+			parsed_com[i+1] = "";
+			i += 2;
+		}
+		else if ( strcmp( (char*)&parsed_com[i][0], "<<" ) == 0 )
+		{	// Input redirection
+			int inp = open( (char*)&parsed_com[i+1][0], O_RDONLY );
+			if ( inp < 0 ){
+
+				perror("minsh");
+				return 1;
+			}
+
+			if ( dup2(inp, 0) < 0 ){
+				perror("minsh");
+				return 1;
+			}
+			close(inp);
+			parsed_com[i] = "";
+			parsed_com[i+1] = "";
+			i += 2;
+		}
+		else if( strcmp( (char*)&parsed_com[i][0], ">") == 0 )
+		{	// Output redirection
+
+			int out = open( (char*)&parsed_com[i+1][0], O_WRONLY | O_TRUNC | O_CREAT, 0755 );
+			if ( out < 0 ){
+				perror("minsh");
+				return 1;
+			}
+
+			if ( dup2(out, 1) < 0 ){
+				perror("minsh");
+				return 1;
+			}
+			close(out);
+			parsed_com[i] = "";
+			parsed_com[i+1] = "";
+			i += 2;
+		}
+		else if( strcmp( (char*)&parsed_com[i][0], ">>") == 0 )
+		{	// Output redirection (append)
+			int out = open( (char*)&parsed_com[i+1][0], O_WRONLY | O_APPEND | O_CREAT, 0755 );
+			if ( out < 0 ){
+				perror("minsh");
+				return 1;
+			}
+
+			if ( dup2(out, 1) < 0 ){
+				perror("minsh");
+				return 1;
+
+			}
+			close(out);
+			parsed_com[i] = "";
+			parsed_com[i+1] = "";
+			i += 2;
+		}
+		else if( strcmp( (char*)&parsed_com[i][0], "2>") == 0 )
+		{	// Error redirection
+			int err = open( (char*)&parsed_com[i+1][0], O_WRONLY | O_CREAT, 0755 );
+			if ( err < 0 ){
+				perror("minsh");
+				return 1;
+			}
+
+			if ( dup2(err, 2) < 0 ){
+				perror("minsh");
+				return 1;
+			}
+			close(err);
+			parsed_com[i] = "";
+			parsed_com[i+1] = "";
+			i += 2;
+		}
+		else if( strcmp( (char*)&parsed_com[i][0], "2>>") == 0 )
+		{	// Error redirection
+			int err = open( (char*)&parsed_com[i+1][0], O_WRONLY | O_CREAT | O_APPEND, 0755 );
+
+			if ( err < 0 ){
+				perror("minsh");
+				return 1;
+			}
+
+			if ( dup2(err, 2) < 0 ){
+				perror("minsh");
+				return 1;
+			}
+			close(err);
+			parsed_com[i] = "";
+			parsed_com[i+1] = "";
+			i += 2;
+
+		}
+		else{
+			i++;
+		}
+	}
+
+	// For other commands, execute a child process
+	launch_proc(parsed_com);
+
+	// Restore the Standard Input and Output file descriptors
+	dup2(std_in, 0);
+	dup2(std_out, 1);
+	dup2(std_err, 2);
+	return 1;
+}
+
+void launch_proc_pipe(string **parsed_pipe, int pipe_val)
+{
+	int *fd1 = new int[2*pipe_val-2];  // Used to store two ends of first pipe 
+	int status;
+    pid_t p = 1;
+  	
+    for(int i=0;i<pipe_val-1;i++)
+    {
+    	pipe(fd1+2*i);
+    }
+
+    int std_in, std_out, std_err;
+	std_in = dup(0);
+	std_out = dup(1);
+	std_err = dup(2);
+    
+	p = fork();
+	if(p==0)
+	{	
+		dup2(fd1[1],1);
+		for(int i=0;i<2*pipe_val-2;i++)
+	    {	if(i==1) continue;
+	    	close(fd1[i]);
+	    }
+		
+		launch_proc(parsed_pipe[0]);
+
+		close(fd1[1]);
+		
+		exit(0);
+    }
+    else
+    {	
+		
+	    wait(NULL);
+	    int j=1;
+	    for(int k=1;k<pipe_val-1;k++)
+	    {	
+	    	
+	    	
+	    	j++;
+
+	    	p = fork(); 
+
+		    if (p < 0) 
+		    { 
+		        fprintf(stderr, "fork Failed" ); 
+		        exit(1); 
+		    } 
+		  	
+		  	if(p==0)
+		  	{
+		  		
+		  		dup2(fd1[2*k-2],0);
+	  			dup2(fd1[2*k+1],1);
+		  		for(int i=0;i<2*pipe_val-2;i++)
+		    	{	
+		    		if((i==2*k-2) || (i==2*k+1)) continue;
+		    		close(fd1[i]);
+		    	}
+		        
+		    	
+
+
+		        launch_proc(parsed_pipe[k]);
+		        close(fd1[2*k-2]);
+		        close(fd1[2*k+1]);
+		  
+		        exit(0); 
+		    
+	    	}
+	    	else
+	    	{
+	    		wait(NULL);	
+		    	
+	    	}
+	    }
+
+	    p = fork(); 
+
+	    if (p < 0) 
+	    { 
+	        fprintf(stderr, "fork Failed" ); 
+	        exit(1); 
+	    } 
+	  	
+	  	if(p==0)
+	  	{
+
+	  		
+
+	  		for(int i=0;i<2*pipe_val-2;i++)
+	    	{	
+	    		if(i==2*j-2)
+	    			continue;
+	    		close(fd1[i]);
+	    	}
+	        
+	    	dup2(fd1[2*j-2],0);
+			dup2(std_out,1);
+
+	        launch_proc(parsed_pipe[j]);
+	        close(fd1[2*j-2]);
+
+	        exit(0);  
+	    
+		}
+		else
+		{
+			
+			for(int i=0;i<2*pipe_val-2;i++)
+			{
+				close(fd1[i]);
+			}
+			wait(NULL);
+			dup2(std_in, 0);
+			dup2(std_out, 1);
+			dup2(std_err, 2);
+
+		    
+		    return;	
+		}
+	}
+	
+}
+
+int main()
+{
+
+	string input;
+	string *parsed_com, **parsed_pipe;
+ 
+	int i=1,pipe_val=0;
+	
+	while(1)
+	{	
+		parsed_com = new string[MAX_LENGTH];
+		parsed_pipe = new string*[MAX_LENGTH];
+		print_direc();
+		getline(cin,input);
+		if(input.size()==0)
+		{	
+			continue;
+		}
+
+		pipe_val = split_fn(input,parsed_com,parsed_pipe);	
+		
+
+		if(pipe_val==0)
+		{	int chk = 0;
+			for(int i=1;i<MAX_LENGTH;i++)
+			{
+				if(strcmp((char*)&parsed_com[i][0],"<")==0 || strcmp((char*)&parsed_com[i][0],">")==0 || strcmp((char*)&parsed_com[i][0],">>")==0)
+				{
+					chk ++;
+					break;
+				}
+			}
+
+			if(chk)
+			{	
+				
+				int a = launch_io_redirect(parsed_com);
+			}
+			else
+			{
+				launch_proc(parsed_com);
+			}
+		}
+		else
+		{
+		
+			launch_proc_pipe(parsed_pipe,pipe_val);
+		}
+		
+		
+	}
+
+	return 0;
+}
