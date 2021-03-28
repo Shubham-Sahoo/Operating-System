70321b4a46902f6d0c4754d50950f862e28bd6b0
thelonewolf1603 <47342213+thelonewolf1603@users.noreply.github.com>
2021-03-28 20:24:46 +0530

    Update syscall.c

--- Assignment-6/syscall.c
+++ Assignment-6/syscall.c
@@ -1,155 +1,392 @@
-#include "userprog/process.h"
 #include "userprog/syscall.h"
 #include <stdio.h>
 #include <syscall-nr.h>
-#include <console.h>
+#include <devices/shutdown.h>
+#include <threads/vaddr.h>
+#include <filesys/filesys.h>
+#include <string.h>
+#include <filesys/file.h>
+#include <devices/input.h>
+#include <threads/palloc.h>
+#include <threads/malloc.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
-#include "filesys/file.h"
-#include "filesys/filesys.h"
-#include "devices/input.h"
+#include "process.h"
+#include "pagedir.h"
+
 static void syscall_handler (struct intr_frame *);
-static void sys_exit(struct intr_frame *f_)
+
+void syscall_halt(void);
+void syscall_exit(int *sp);
+int syscall_exec(int *sp);
+int syscall_wait(int *sp);
+
+int syscall_create(int *sp);
+int syscall_remove(int *sp);
+int syscall_open(int *sp);
+int syscall_filesize(int *sp);
+int syscall_read(int *sp);
+int syscall_write(int *sp);
+void syscall_seek(int *sp);
+int syscall_tell(int *sp);
+void syscall_close(int *sp);
+void is_valid_vaddr(const void *vaddr);
+void exit(int exit_code);
+struct opened_file *search_file(int fd);
+
+void exit(int exit_code)
 {
-  unsigned int *esp = f_->esp;
-  int status = *(esp + 1);
-  struct thread *t = thread_current();
-  printf("(%s) EXIT : %s\n", t->name, status ? "EXIT_FAILURE" :  "EXIT_SUCCESS");
-  thread_exit ();
+  thread_current()->exit_code = exit_code;
+  thread_exit();
 }
 
-static void sys_exec(struct intr_frame *f_)
-{
-  unsigned int *esp = f_->esp;
-  const char *file = *(esp + 1);
-  
-  f_->eax = process_execute(file);
+void unexpected_exit(){
+  exit(-1);
 }
 
-static void sys_wait(struct intr_frame *f_)
+void is_valid_vaddr(const void *vaddr)
 {
+  if (vaddr == NULL)
+    unexpected_exit();
+
+  //access kernel page
+  if (!is_user_vaddr(vaddr) || !is_user_vaddr(vaddr + 4))
+    unexpected_exit();
+
+  //unmapped page
+  if (pagedir_get_page(thread_current()->pagedir, vaddr) == NULL)
+    unexpected_exit();
 }
 
-static void sys_create(struct intr_frame *f_)
+/*
+  get 4 byte information.
+  the unit of offset is word(4 byte), or just consider as the arg index in stack sp
+*/
+void get_stack_elem(int *sp, int *val, int offset)
 {
-  unsigned int *esp = f_->esp;
-  char *file = *(esp + 1);
-  unsigned int initial_size = *(esp + 2);
-  
-  f_->eax = filesys_create(file, initial_size);
+  //note: beware of invalid access
+  is_valid_vaddr(sp + offset);
+  *val = *(sp + offset);
 }
 
-static void sys_remove(struct intr_frame *f_)
+struct opened_file *search_file(int fd)
 {
-  unsigned int *esp = f_->esp;
-  char *file = *(esp + 1);
-  
-  f_->eax = filesys_remove(file);
-}
+  struct list_elem *elem;
+  struct opened_file *of = NULL;
+  struct thread *cur = thread_current();
 
-static void sys_open(struct intr_frame *f_)
-{
-  unsigned int *esp = f_->esp;
-  char *file = *(esp + 1);
-  struct file *f = filesys_open(file);
-    
-  if (f == NULL) f_->eax = -1;
-  else 
+  for (elem = list_begin(&cur->files);
+       elem != list_end(&cur->files);
+       elem = list_next(elem))
   {
-    f_->eax = file_insert_in_fd(f);
+    of = list_entry(elem, struct opened_file, file_elem);
+    if (of->fd == fd)
+      return of;
   }
+  return NULL;
 }
 
-static void sys_filesize(struct intr_frame *f_)
+void syscall_init(void)
 {
-  unsigned int *esp = f_->esp;
-  int fd = *(esp + 1);
-  struct file *f = file_search_in_fd(fd);
-    
-  if (f == NULL) f_->eax = -1;
-  else f_->eax = file_length(f);  
-}
-
-static void sys_read(struct intr_frame *f_)
-{
-  unsigned int *esp = f_->esp;
-  int fd = *(esp + 1);
-  char *buffer = *(esp + 2);
-  unsigned int size = *(esp + 3);
-  struct file *f;
-  
-  if (fd == STDIN_FILENO) f_->eax = input_getc();
-  else if (fd < 3) f_->eax = -1;
-  else
-  {
-    f = file_search_in_fd(fd);
-    if (f == NULL) f_->eax = -1;
-    else f_->eax = file_read(f, buffer, size);
-  }
-}
-
-static void sys_write(struct intr_frame *f_)
-{
-  unsigned int *esp = f_->esp;
-  int fd = *(esp + 1);
-  char *buffer = *(esp + 2);
-  unsigned int size = *(esp + 3);
-  struct file *f;
-  
-  if (fd == STDOUT_FILENO) putbuf(buffer, size);
-  else if (fd < 3) f_->eax = -1;
-  else
-  {
-    f = file_search_in_fd(fd);
-    if (f == NULL) f_->eax = -1;
-    else f_->eax = file_write(f, buffer, size);
-  }
-}
-void
-syscall_init (void) 
-{
-  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+  intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler(struct intr_frame *f UNUSED)
 {
-  // get the syscall number
-  int syscall_num = *(int*) (f->esp);
-  uint8_t *ptr = (uint8_t*) (f->esp);
-  char c;
-	
-	unsigned int *esp;
-struct file *p;
-char *filename;
-  // arguments to write
+  int *sp = f->esp;
+  is_valid_vaddr(sp);
+
+  //stack top is NUMBER
+  int syscall_type = (*sp);
+  switch (syscall_type)
+  {
+  case SYS_HALT:
+    syscall_halt();
+    break;
+  case SYS_EXIT:
+    syscall_exit(sp);
+    break;
+  case SYS_EXEC:
+    f->eax = syscall_exec(sp);
+    break;
+  case SYS_WAIT:
+    f->eax = syscall_wait(sp);
+    break;
+  case SYS_CREATE:
+    f->eax = syscall_create(sp);
+    break;
+  case SYS_REMOVE:
+    f->eax = syscall_remove(sp);
+    break;
+  case SYS_OPEN:
+    f->eax = syscall_open(sp);
+    break;
+  case SYS_FILESIZE:
+    f->eax = syscall_filesize(sp);
+    break;
+  case SYS_READ:
+    f->eax = syscall_read(sp);
+    break;
+  case SYS_WRITE:
+    f->eax = syscall_write(sp);
+    break;
+  case SYS_SEEK:
+    syscall_seek(sp);
+    break;
+  case SYS_TELL:
+    f->eax = syscall_tell(sp);
+    break;
+  case SYS_CLOSE:
+    syscall_close(sp);
+    break;
+
+  default:
+    printf("unknown syscall: %d\n", syscall_type);
+    break;
+  }
+}
+
+void syscall_halt(void)
+{
+  shutdown_power_off();
+}
+
+void syscall_exit(int *sp)
+{
+  int exit_code;
+  struct thread *cur = thread_current();
+  get_stack_elem(sp, &exit_code, 1);
+
+  cur->exit_code = exit_code;
+
+  thread_exit();
+}
+
+int syscall_exec(int *sp)
+{
+  char *file_name;
+  get_stack_elem(sp, &file_name, 1);
+  is_valid_vaddr(file_name);
+
+  return process_execute(file_name);
+}
+
+int syscall_wait(int *sp)
+{
+  tid_t child_id;
+  get_stack_elem(sp, &child_id, 1);
+  return process_wait(child_id);
+}
+
+//don't use bool for 32-bit align
+int syscall_create(int *sp)
+{
+  bool success;
+  int initial_size;
+  char *file_name;
+
+  get_stack_elem(sp, &file_name, 1);
+  get_stack_elem(sp, &initial_size, 2);
+
+  is_valid_vaddr(file_name);
+
+  acquire_file_lock();
+  success = filesys_create(file_name, initial_size);
+  release_file_lock();
+
+  return success;
+}
+int syscall_remove(int *sp)
+{
+  bool success;
+  char *file_name;
+
+  get_stack_elem(sp, &file_name, 1);
+  is_valid_vaddr(file_name);
+
+  acquire_file_lock();
+  success = filesys_remove(file_name);
+  release_file_lock();
+  
+  return success;
+}
+
+int syscall_open(int *sp)
+{
+  int fd;
+  char *file_name;
+
+  get_stack_elem(sp, &file_name, 1);
+  is_valid_vaddr(file_name);
+
+  struct thread *cur = thread_current();
+
+  acquire_file_lock();
+  struct file *file = filesys_open(file_name);
+  release_file_lock();
+
+  if (file != NULL)
+  {
+    struct opened_file *of = malloc(sizeof(struct opened_file));
+    of->fd = cur->fd_cnt++;
+    of->file = file;
+    list_push_back(&cur->files, &of->file_elem);
+    fd = of->fd;
+  }
+  else
+  {
+    fd = -1;
+  }
+
+  return fd;
+}
+
+int syscall_filesize(int *sp)
+{
+  int filesize;
+  int fd;
+
+  get_stack_elem(sp, &fd, 1);
+
+  struct opened_file *of = search_file(fd);
+
+  if (of != NULL)
+  {
+    acquire_file_lock();
+    filesize = file_length(of->file);
+    release_file_lock();
+  }
+  else
+  {
+    filesize = -1;
+  }
+  return filesize;
+}
+
+int syscall_read(int *sp)
+{
+  int readsize;
   int fd;
   char *buffer;
-  unsigned size;
+  int size;
 
-  // arguments to exec
-  char *progname;
+  get_stack_elem(sp, &fd, 1);
+  get_stack_elem(sp, &buffer, 2);
+  get_stack_elem(sp, &size, 3);
 
-  switch(syscall_num) {
-	case SYS_WRITE:
-		// get arguments from stack
-		sys_write(f);
-	break;
+  is_valid_vaddr(buffer);
 
-	case SYS_EXIT:
-		sys_exit(f);
-	break;
+  if (fd == 0)
+  {
+    for (int i = 0; i < size; i++)
+      buffer[i] = input_getc();
 
-	case SYS_READ:
-		sys_read(f);
-	break;
+    readsize = size;
+  }
+  else
+  {
+    struct opened_file *of = search_file(fd);
+    if (of != NULL)
+    {
+      acquire_file_lock();
+      readsize = file_read(of->file, buffer, size);
+      release_file_lock();
+    }
+    else
+    {
+      readsize = -1;
+    }
+  }
+  return readsize;
+}
+int syscall_write(int *sp)
+{
+  int writesize;
+  int fd;
+  void *buffer;
+  int size;
 
-	case SYS_EXEC:
-		sys_exec(f);
+  get_stack_elem(sp, &fd, 1);
+  get_stack_elem(sp, &buffer, 2);
+  get_stack_elem(sp, &size, 3);
 
-	break;
-	case SYS_OPEN:
-		sys_open(f);
-			break;
-  } 
+  is_valid_vaddr(buffer);
+
+  if (fd == 1)
+  {
+    putbuf(buffer, size);
+    writesize = size;
+  }
+  else
+  {
+    struct opened_file *of = search_file(fd);
+    if (of != NULL)
+    {
+      acquire_file_lock();
+      writesize = file_write(of->file, buffer, size);
+      release_file_lock();
+    }
+    else
+    {
+      writesize = 0;
+    }
+  }
+
+  return writesize;
+}
+void syscall_seek(int *sp)
+{
+  int fd;
+  int position;
+
+  get_stack_elem(sp, &fd, 1);
+  get_stack_elem(sp, &position, 2);
+
+  struct opened_file *of = search_file(fd);
+
+  if (of != NULL)
+  {
+    acquire_file_lock();
+    file_seek(of->file, position);
+    release_file_lock();
+  }
+}
+int syscall_tell(int *sp)
+{
+  int pos;
+  int fd;
+
+  get_stack_elem(sp, &fd, 1);
+
+  struct opened_file *of = search_file(fd);
+  if (of != NULL)
+  {
+    acquire_file_lock();
+    pos = file_tell(of->file);
+    release_file_lock();
+  }
+  else
+  {
+    pos = -1;
+  }
+
+  return pos;
+}
+
+void syscall_close(int *sp)
+{
+  int fd;
+  get_stack_elem(sp, &fd, 1);
+
+  struct opened_file *of = search_file(fd);
+  if (of != NULL)
+  {
+    acquire_file_lock();
+    file_close(of->file);
+    release_file_lock();
+
+    list_remove(&of->file_elem);
+    free(of);
+  }
 }
