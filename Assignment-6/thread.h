31fee151c78c2da1057711cac610601a9cceb2ad
thelonewolf1603 <47342213+thelonewolf1603@users.noreply.github.com>
2021-03-28 22:18:25 +0530

    Update thread.h

--- Assignment-6/thread.h
+++ Assignment-6/thread.h
@@ -4,7 +4,7 @@
 #include <debug.h>
 #include <list.h>
 #include <stdint.h>
-#include <threads/synch.h>
+#include "threads/synch.h"
 
 /* States in a thread's life cycle. */
 enum thread_status
@@ -26,13 +26,11 @@ typedef int tid_t;
 #define PRI_MAX 63                      /* Highest priority. */
 
 /* A kernel thread or user process.
-
    Each thread structure is stored in its own 4 kB page.  The
    thread structure itself sits at the very bottom of the page
    (at offset 0).  The rest of the page is reserved for the
    thread's kernel stack, which grows downward from the top of
    the page (at offset 4 kB).  Here's an illustration:
-
         4 kB +---------------------------------+
              |          kernel stack           |
              |                |                |
@@ -54,22 +52,18 @@ typedef int tid_t;
              |               name              |
              |              status             |
         0 kB +---------------------------------+
-
    The upshot of this is twofold:
-
       1. First, `struct thread' must not be allowed to grow too
          big.  If it does, then there will not be enough room for
          the kernel stack.  Our base `struct thread' is only a
          few bytes in size.  It probably should stay well under 1
          kB.
-
       2. Second, kernel stacks must not be allowed to grow too
          large.  If a stack overflows, it will corrupt the thread
          state.  Thus, kernel functions should not allocate large
          structures or arrays as non-static local variables.  Use
          dynamic allocation with malloc() or palloc_get_page()
          instead.
-
    The first symptom of either of these problems will probably be
    an assertion failure in thread_current(), which checks that
    the `magic' member of the running thread's `struct thread' is
@@ -81,6 +75,9 @@ typedef int tid_t;
    only because they are mutually exclusive: only a thread in the
    ready state is on the run queue, whereas only a thread in the
    blocked state is on a semaphore wait list. */
+struct list files;
+struct semaphore filesync;
+struct semaphore ex;
 struct thread
   {
     /* Owned by thread.c. */
@@ -89,13 +86,10 @@ struct thread
     char name[16];                      /* Name (for debugging purposes). */
     uint8_t *stack;                     /* Saved stack pointer. */
     int priority;                       /* Priority. */
+    int maxfd;
     struct list_elem allelem;           /* List element for all threads list. */
-    int64_t wakeup_time;
-
-    /* Shared between thread.c and synch.c. */
-    struct list_elem elem;              /* List element. */
-    struct list_elem timer_elem;
-  struct list childs;
+    //bool fl;
+    struct list childs;
     int exec;
     int exit;
     int parentID;
@@ -110,27 +104,6 @@ struct thread
     /* Owned by thread.c. */
     unsigned magic;                     /* Detects stack overflow. */
   };
-
-/*
-  used for syscall "wait"
-*/
-struct child_info
-{
-  struct list_elem child_elem;
-  tid_t tid;
-  int exit_code;
-  bool is_exit;
-  bool bewaited;
-  struct thread *child; // only used for remove_all_children_info(); dealed with orphaned process problem
-  struct semaphore wait_sema;
-};
-
-struct opened_file
-{
-  int fd;
-  struct file *file;
-  struct list_elem file_elem;
-};
 
 /* If false (default), use round-robin scheduler.
    If true, use multi-level feedback queue scheduler.
@@ -167,19 +140,5 @@ int thread_get_nice (void);
 void thread_set_nice (int);
 int thread_get_recent_cpu (void);
 int thread_get_load_avg (void);
-bool wakeup_inorder (const struct list_elem *left, const struct list_elem *right, void *aux UNUSED);
-
-void acquire_file_lock(void);
-void release_file_lock(void);
-bool held_file_lock(void);
-
-
-void close_all_files(struct list* files);
-
-struct child_info *get_child_info(struct thread *th,tid_t tid);
-void add_child_info(struct thread *th,struct child_info *ch);
-void remove_child_info(struct thread *th, tid_t tid);
-void remove_all_children_info(struct thread *th);
-
 
 #endif /* threads/thread.h */
