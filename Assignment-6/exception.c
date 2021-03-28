af20644b104934faa82a1d39098b4e30aa20f0fe
thelonewolf1603 <47342213+thelonewolf1603@users.noreply.github.com>
2021-03-28 20:33:15 +0530

    Update exception.c

--- Assignment-6/exception.c
+++ Assignment-6/exception.c
@@ -2,6 +2,7 @@
 #include <inttypes.h>
 #include <stdio.h>
 #include "userprog/gdt.h"
+#include "userprog/syscall.h"
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 
@@ -148,6 +149,7 @@ page_fault (struct intr_frame *f)
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
 
+     unexpected_exit();
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
      which fault_addr refers. */
