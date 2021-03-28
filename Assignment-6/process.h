422eb24df922b943dd851ea46f57543383a5515f
thelonewolf1603 <47342213+thelonewolf1603@users.noreply.github.com>
2021-03-28 21:45:31 +0530

    Update process.h

--- Assignment-6/process.h
+++ Assignment-6/process.h
@@ -7,5 +7,25 @@ tid_t process_execute (const char *file_name);
 int process_wait (tid_t);
 void process_exit (void);
 void process_activate (void);
+struct child {
+
+	struct list_elem e;
+	struct thread *t;
+	int childstatus;
+	int threadid;
+	bool fl;
+};
+
+struct filefd {
+
+	int fd;
+	struct list_elem e;
+	struct file *f;
+	int tid;
+	struct thread *fdt;
+
+};
+
+//static void start_process (void *);
 
 #endif /* userprog/process.h */
